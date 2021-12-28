571ba0fd9398048f76c49c8f3868bdbbf95ec4a2
Sebastian <roshfeltsemo@gmail.com>
2021-12-28 07:41:51 -0500

    session 4

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const Person = require('./person');
+// Example for person.js
+// const Person = require('./person');
 
-const person1 = new Person('Sebastian', 22);
+// const person1 = new Person('Sebastian', 22);
 
-person1.greeting();
+// person1.greeting();
+
+// Example for logger.js
+const Logger = require('./logger');
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called Listener', data));
+
+logger.log('Hello Events!');
