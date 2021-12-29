ba9d612152aa72948d096bdcdd8fe5459f5e2e9d
Sebastian <roshfeltsemo@gmail.com>
2021-12-29 07:39:25 -0500

    session 4

--- index.js
+++ index.js
@@ -5,10 +5,29 @@
 
 // person1.greeting();
 
+
+
+
 // Example for logger.js
-const Logger = require('./logger');
-const logger = new Logger();
+// const Logger = require('./logger');
+// const logger = new Logger();
 
-logger.on('message', (data) => console.log('Called Listener', data));
+// logger.on('message', (data) => console.log('Called Listener', data));
 
-logger.log('Hello Events!');
+// logger.log('Hello Events!');
+
+
+// Final example
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.end('<h1>Hello World!</h1>'); 
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
