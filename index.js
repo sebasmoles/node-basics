ae6b41235aa429f7ffa65045e56174f769b5774e
Sebastian <roshfeltsemo@gmail.com>
2022-01-03 06:02:11 -0500

    session 5

--- index.js
+++ index.js
@@ -23,9 +23,74 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        res.end('<h1>Hello World!</h1>'); 
+
+    // Not optimal way:
+
+    // if(req.url === '/') {
+    //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+    //         if (err) throw err;
+    //         res.writeHead(200, { 'Content-Type': 'text/html' })
+    //         res.end(content); 
+    //     });
+    // }
+
+    // if(req.url === '/about') {
+    //     fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+    //         if (err) throw err;
+    //         res.writeHead(200, { 'Content-Type': 'text/html' })
+    //         res.end(content); 
+    //     });
+    // }
+
+    // if(req.url === '/api/users') {
+    //     const users = [
+    //         { name: 'Bob Smith', age: 40 },
+    //         { name: 'John Doe', age: 30 }
+    //     ]
+
+    //     res.writeHead(200, { 'Content-Type': 'application/json' });
+    //     res.end(JSON.stringify(users));
+    // }
+
+    // Optimal way: 
+    
+    // Build file dynamic path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+    // Extension of file
+    let extname = path.extname(filePath);    
+
+    // Initial content type
+    let contentType = 'text/html';
+
+    // Check ext and set content type
+    switch(extname) {
+        case ".js":
+            contentType = "text/javascript";
+            break;
+        case ".css":
+            contentType = "text/css";
+            break;
+        case ".json":
+            contentType = "application/json";
+            break;
+        case ".png":
+            contentType = "image/png";
+            break;
+        case ".jpg":
+            contentType = "image/jpg";
+            break;        
     }
+
+    // Read file
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code == 'ENOENT') {
+                // Page not found
+            }
+        }
+
+    })
 });
 
 const PORT = process.env.PORT || 5000;
